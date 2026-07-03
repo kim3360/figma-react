6c3f5aaa4566a5e7fe9154543a5da1ce0b924dcb
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-07-03 15:45:24 +0900

    fix : vercel 파일 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router';
-import MainContainer from '@/components/layout/main/MainContainer';
+import { createFileRoute } from "@tanstack/react-router"
+import MainContainer from "@/components/layout/main/MainContainer"
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
-  return <MainContainer />;
+  return <MainContainer />
 }
